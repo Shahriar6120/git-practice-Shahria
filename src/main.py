1e945524564c13147d5f32e46d5c500d24709af5
Shahriar6120 <mdshahriar6120@gmail.com>
2026-08-29 22:56:21 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 from datetime import date
+from utils import add, subtract
+
 print(f"Name : Shahria")
 print("Date : ", date.today())
+
+number1 = float(input("Enter the First Number : "))
+number2 = float(input("Enter the Second Number : "))
+
+print("Addition : ", add(number1, number2))
+print("Subtraction : ",subtract(number1 , number2))
+ 
